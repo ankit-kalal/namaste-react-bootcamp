3f69c6b068f9db245f6453296e4ffe8edbde02a3
ankit <ankitkalal1502@gmail.com>
2023-09-16 12:58:57 +0530

    started react with parcel day 2

--- App.js
+++ App.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
 const heading = React.createElement("h1",{},"test text here 123")
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading)
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # namaste-react-bootcamp
+HMR- Hot Module Reloading
+File Watcher algorithm -C++
